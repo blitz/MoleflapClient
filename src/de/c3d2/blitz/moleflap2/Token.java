2210ccc50ee6eeb2f2bc89758f737479f7cde3a9
Julian Stecklina <js@alien8.de>
2011-07-24 17:25:56 +0200

    Try a bit harder to interpret a file as token.

--- src/de/c3d2/blitz/moleflap2/Token.java
+++ src/de/c3d2/blitz/moleflap2/Token.java
@@ -1,8 +1,13 @@
 package de.c3d2.blitz.moleflap2;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+
+import android.util.Log;
 
 class Token {	
     static final int TOKEN_LENGTH = 164;
@@ -20,16 +25,19 @@ class Token {
 	}
 	
 	public static Token fromFile(File file) throws IOException {
-        FileReader fr = new FileReader(file);
-        char b[] = new char[TOKEN_LENGTH];
-        int len = 0;
-        try {
-        	len = fr.read(b, 0, TOKEN_LENGTH);
-        	if (len != TOKEN_LENGTH)
-        		throw new IOException();
-        } finally {        	
-        	fr.close();
-        }
-		return new Token(new String(b));
+		Log.d(MoleflapClient.TAG, "Tring to import " + file.toString());
+		BufferedReader input =  new BufferedReader(new FileReader(file));
+		String line = null;
+		int ignored = 0;
+		
+		while  ((line = input.readLine()) != null) {
+			if (line.length() == TOKEN_LENGTH) {
+				Log.d(MoleflapClient.TAG, "Succeeded. Ignored " + ignored + " line(s).");
+				return new Token(line);
+			} else ignored++;			
+		}
+		
+		Log.d(MoleflapClient.TAG, "No valid token found.");
+		throw new IOException();
 	}
 }
